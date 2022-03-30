21955ad1b91446bfcf72bfd182dea54ba4dbdbcc
neeber <chatextazy@gmail.com>
2022-03-30 21:06:17 +0700

    upload

--- client/src/components/Geography/Geography.jsx
+++ client/src/components/Geography/Geography.jsx
@@ -2,37 +2,44 @@ import React from "react";
 import PreviewText from "../PreviewText/PreviewText";
 import marker from "@images/Geography/map-marker.svg";
 import RussiaMap from "./RussiaMap";
+import ScrollTrggier from "react-scroll-trigger"
 
 const Geography = () => {
+  const [mapActive, setMapActive] = React.useState(false)
+
   return (
-    <section id="geography">
-      <div className="container">
-        <div className="geography__inside">
-          <div className="geography__adress">
-            <div className="colum">
-              <PreviewText value="География работы" />
-              <div className="geography__title">
-                <span>Красноярский край</span>&nbsp; Иркутская область
-                Республика хакасия
-              </div>
-              <div className="geography__location">
-                <div className="geography__location-marker">
-                  <img src={marker} alt="Красноярск" />
+    <ScrollTrggier onEnter={() => setMapActive(true)}>
+      <section id="geography">
+        <div className="container">
+          <div className="geography__inside">
+            <div className="geography__adress">
+              <div className="colum">
+                <PreviewText value="География работы" />
+                <div className="geography__title">
+                  <div className="krsk">Красноярский край</div>
+                  <div className="irkutsk">Иркутская область</div>
+                  <div className="hakasia">Республика хакасия</div>
                 </div>
-                <div className="geography__location-title">
-                  <span>Головной офис</span>
-                  Красноярск
+                <div className="geography__location">
+                  <div className="geography__location-marker">
+                    <img src={marker} alt="Красноярск" />
+                  </div>
+                  <div className="geography__location-title">
+                    <span>Головной офис</span>
+                    Красноярск
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="geography__map">
-            <RussiaMap />
+            <div className="geography__map">
+              <RussiaMap active={mapActive} />
+            </div>
+
           </div>
         </div>
-      </div>
-      <div className="galleryBg"></div>
-    </section>
+        <div className="galleryBg"></div>
+      </section>
+    </ScrollTrggier >
   );
 };
 
