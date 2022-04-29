5a36d06e1758d34b3184e4b157fc1ea099c100f1
40thz <68428668+40thz@users.noreply.github.com>
2022-04-29 12:46:47 +0700

    upload

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const buildAdminRouter = require("./admin");
 const vacancyRouter = require("./vacancy");
 const mailRouter = require("./mail");
-//mail
+
 module.exports = {
   buildAdminRouter,
   vacancyRouter,
