b1d08ef843ae05d0e0b3689f1034061ff24860d2
40thz <68428668+40thz@users.noreply.github.com>
2022-06-03 15:59:57 +0700

    showreal

--- client/src/components/Showreal/Showreal.jsx
+++ client/src/components/Showreal/Showreal.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-import Iframe from "react-iframe";
-// import sample from "@images/showreal.mp4";
+import sample from "@images/showreal.mp4";
 const Showreal = () => {
   return (
     <div className="showreal">
-      <div id="video">
-        <iframe
-          src="//www.youtube.com/embed/40g9QKsfBJM?autoplay=1&mute=1&loop=1&playlist=40g9QKsfBJM"
-          name="youtube embed"
-          allow="autoplay; encrypted-media"
-          allowfullscreen
-          width="100%"
-        ></iframe>
-      </div>
+      <video autoPlay loop muted>
+        <source src={sample} type="video/mp4" />
+      </video>
     </div>
   );
 };
