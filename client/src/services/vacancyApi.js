0202e7637ad31f8066222a6e6171ba8fb32201be
40thz <68428668+40thz@users.noreply.github.com>
2022-06-08 18:29:27 +0700

    fix

--- client/src/services/vacancyApi.js
+++ client/src/services/vacancyApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const vacancyApi = createApi({
   reducerPath: "vacancyApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/",
+    baseUrl: "https://regionsnab24.ru/",
   }),
   endpoints: (build) => ({
     getVacancy: build.query({
